970bb226a5c91a9d49c6e229dce6c914ad1a5232
Collins <jbcollins254@gmail.com>
2025-05-23 13:40:31 +0300

    updates error3

--- src/pages/PaymentStatus.tsx
+++ src/pages/PaymentStatus.tsx
@@ -132,7 +132,7 @@ const PaymentStatus = () => {
             
             <div className="flex flex-col sm:flex-row gap-4 justify-center">
               <Button 
-                onClick={() => navigate('/tickets')} 
+                onClick={() => navigate('/dashboard')} 
                 className="bg-green-600 hover:bg-green-700 text-white"
               >
                 View My Tickets
